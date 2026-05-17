1ea9583f19cc104a5283f2d78433feb3ece3d30a
Abdul Ahad Quraishi <abdulahadq4033@gmail.com>
2026-05-17 21:54:06 +0530

    pages functionality

--- Projects/PORTFOLIO/script.js
+++ Projects/PORTFOLIO/script.js
@@ -3,4 +3,14 @@ window.addEventListener("load", function(){
     setTimeout(function(){
         loader.classList.add("fade-out");
     }, 2000);
+
 });
+
+
+function showTab(tabId){
+    const tabs = document.querySelectorAll(".tab");
+    tabs.forEach(tab => {
+        tab.classList.remove("active");
+    });
+    document.getElementById(tabId).classList.add("active");
+}
